fd5be9d9353d82fc6d78735a217b11a5f1086fd6
unk <unk@unk.unk>
2025-04-03 11:26:27 +0000

    Feat LogOut Method With Clearing Cookies - NEAR-8

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->string('password');
             $table->enum('role', ['User', 'Admin'])->default('User');
             $table->string('refresh_token')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
 
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\{AuthController};
+use App\Http\Middleware\JWTGuard;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('/signup', [AuthController::class, 'store']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::group(['middleware' => JWTGuard::class], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
