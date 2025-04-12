0c3e75648291e966779d9a0242b16e35ebcc8ab7
Amanar Marouane <marouane.amanar07@gmail.com>
2025-04-12 21:44:47 +0100

    Improvements On IsLogged Middleware

--- app/Http/Middleware/IsLogged.php
+++ app/Http/Middleware/IsLogged.php
@@ -22,21 +22,22 @@ class IsLogged
     public function handle(Request $request, Closure $next): Response
     {
         $access_token = $request->cookie('access_token');
+        $refresh_token = $request->cookie('refresh_token');
+
         if ($access_token) {
             try {
                 $user = JWTAuth::setToken($access_token)->authenticate();
                 if ($user) {
-                    return $this->error('You are already logged in.', null, [], 403);
+                    return $this->error('Logout first. You are already logged in.', null, [], 403);
                 }
             } catch (JWTException $e) {
             }
         }
 
-        $refresh_token = $request->cookie('refresh_token');
         if ($refresh_token) {
             $user = User::where('refresh_token', $refresh_token)->first();
             if ($user) {
-                return $this->error('You are already logged in.', null, [], 403);
+                return $this->error('Logout first. You are already logged in.', null, [], 403);
             }
         }
 
